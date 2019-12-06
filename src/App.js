d6090dd3ab9891844e1f16a213d921a913661bc9
Ivan Perera <ivan@majestic.com.au>
2019-12-06 16:46:22 +1100

    Add the input button logic and the Clear button logic

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import { Button } from './components/Button/Button.component';
 import { Input } from './components/Input/Input.component';
+import { ClearButton } from './components/ClearButton/ClearButton.component';
 import './App.css';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { input: '' };
+		this.state = {
+			input: '',
+			numbers: [
+				['7', '8', '9', '÷'],
+				['4', '5', '6', 'X'],
+				['1', '2', '3', '+'],
+				['.', '0', '=', '-']
+			]
+		};
 	}
+
+	addToInput = (val) => {
+		//console.log(val.target);
+		this.setState({ input: this.state.input + val });
+	};
 	render() {
-		const numbers = [
-			['7', '8', '9', '÷'],
-			['4', '5', '6', 'X'],
-			['1', '2', '3', '+'],
-			['.', '0', '=', '-']
-		];
 		return (
 			<div className='App'>
 				<div className='calc-wrapper'>
 					<Input input={this.state.input}></Input>
-					{numbers.map((subarray) => (
-						<div className='row'>
-							{subarray.map((x) => (
-								<Button>{x}</Button>
+					{this.state.numbers.map((subarray, idx) => (
+						<div key={idx} className='row'>
+							{subarray.map((x, subIdx) => (
+								<Button
+									key={subIdx}
+									handleClick={this.addToInput}
+								>
+									{x}
+								</Button>
 							))}
 						</div>
 					))}
+					<div className='row'>
+						<ClearButton
+							handleClear={() => this.setState({ input: '' })}
+						>
+							Clear
+						</ClearButton>
+					</div>
 				</div>
 			</div>
 		);
--- src/components/Button/Button.component.jsx
+++ src/components/Button/Button.component.jsx
@@ -10,6 +10,7 @@ export const Button = (props) => (
 		className={`button-wrapper ${
 			isOperator(props.children) ? '' : 'operator'
 		}`}
+		onClick={() => props.handleClick(props.children)}
 	>
 		{props.children}
 	</div>
